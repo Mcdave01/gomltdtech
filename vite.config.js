6dbfa9a6f173634a87b313f343130001a7542bb4
Mcdave01 <mcpointsolutions01@gmail.com>
2025-04-22 07:01:53 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
-  base: '/',
-    build: {
-      outDir: 'dist',
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor'; // Separate vendor libraries into a separate chunk
-            }
+  root: 'src', // Ensure that the root directory is 'src'
+  base: '/', // Define base URL for the project
+  build: {
+    outDir: '../dist', // Output build files to 'dist' outside the 'src' directory
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Split vendor libraries into a separate chunk
           }
         }
       }
-    }// Ensure the output directory is 'dist'
-  
-})
+    }
+  }
+});
