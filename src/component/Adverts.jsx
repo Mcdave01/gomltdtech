757e6582edb51ba7cef62826ed53bb94b62c1e74
mcdave01 <mcpointsolutions01@gmail.com>
2025-04-24 14:57:02 -0700

    update now

--- src/component/Adverts.jsx
+++ src/component/Adverts.jsx
@@ -22,7 +22,7 @@ const Adverts = () => {
   ];
 
   return (
-    <section className="py-12 bg-gray-100">
+    <section className="flex flex-col py-12 bg-gray-100">
       <div className="container mx-auto px-4 sm:px-6">
         <h2 className="text-3xl font-bold text-center mb-8">Our Services</h2>
         <div className="grid grid-cols-1  md:grid-cols-3 gap-8">
